f2f2dd9da48f512a99d1c2731a36cf1cdb97991e
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-04-21 14:38:46 +0200

    add js dyn module function to otoroshi workflows in runtime.d.ts

--- js-runtime/src/runtime.d.ts
+++ js-runtime/src/runtime.d.ts
@@ -4,4 +4,5 @@ declare module 'main' {
   export function cloud_apim_module_plugin_execute_on_backend_call(): I32;
   export function cloud_apim_module_plugin_execute_on_response(): I32;
   export function cloud_apim_module_plugin_execute_on_error(): I32;
+  export function cloud_apim_module_plugin_execute_user_function(): I32;
 }
